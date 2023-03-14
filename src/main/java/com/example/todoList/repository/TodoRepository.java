51fad153c3310d8808f8ad73aaafcb303e95fe1e
Orlando Ferreira <orlandofnfilho@gmail.com>
2023-03-14 08:44:38 -0300

    feat: add h2 database

--- src/main/java/com/example/todoList/repository/TodoRepository.java
+++ src/main/java/com/example/todoList/repository/TodoRepository.java
@@ -3,7 +3,9 @@ package com.example.todoList.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.todoList.model.Todo;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TodoRepository extends JpaRepository<Todo, Long>{
 
 }
